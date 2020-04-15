2e83cdaf4182fda92232dcda558f7d3bf6782168
Charles Patterson Cook <cook.char.p@gmail.com>
2020-04-14 23:39:08 -0400

    Wrote a simple AES object

---
+++ testing/pisgmAES.py
@@ -0,0 +1,30 @@
+from Crypto.Cipher import AES as aes
+from Crypto.Random import get_random_bytes as grab
+
+class aesEncrypter:
+	def __init__(self, key, nonce, message):
+		self.cipher = aes.new(key, aes.MODE_EAX, nonce)
+		if type(message) is str:
+			message = bytes(message, "ascii")
+		self.output = self.cipher.encrypt(message)
+
+class aesDecrypter:
+	def __init__(self, key, nonce, ciphertext):
+		self.cipher = aes.new(key, aes.MODE_EAX, nonce)
+		if type(ciphertext) is str:
+			message = bytes(ciphertext, "ascii")
+		self.output = self.cipher.decrypt(ciphertext)
+
+class aesRadio:
+	def __init__(self, key, nonce=None):
+		self.key = key
+		if nonce == None:
+			self.nonce = grab(16)
+		else:
+			self.nonce = nonce
+
+	def encrypt(self, message):
+		return aesEncrypter(self.key, self.nonce, message).output
+
+	def decrypt(self, ciphertext):
+		return aesDecrypter(self.key, self.nonce, ciphertext).output
