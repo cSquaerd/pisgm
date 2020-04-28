57b5f292ee342d61ab0e3877c1990ab824598e30
Conor Finegan <conor_finegan@student.uml.edu>
2020-04-28 13:58:43 -0400

    client initializes user from test data

--- client/main.py
+++ client/main.py
@@ -1,26 +1,40 @@
-from tkinter import *
+from pathlib import Path
 import json, os, requests, time
 
-SAVE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/user.json"
+MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
+SAVE_PATH = MODULE_DIR + "/user.json"
 
 class User:
-    id = None
-    public_key = None
-    private_key = None
-    ## TODO: More fields? Should the user have a local
-    ## record of what groups he's in?
-    def __init__(self, id, public_key, private_key):
+    def __init__(self, id, public_key, private_key, group):
         self.id = id
         self.public_key = public_key
         self.private_key = private_key
+        self.group = group
+
+def json_to_user(data):
+    id = int(data["id"]);
+    uk = data["public_key"]
+    rk = data["private_key"]
+    group = int(data["group"])
+    return User(id, uk, rk, group)
 
 def new_user():
-    data = {'name': 'hello', 'value': 'world'}
-    ## TODO: Request new user from server.
-    fout = open(SAVE_PATH, "w")
-    ##json.dump(data)
-    fout.close()
-    return data
+    ## TODO: Right now this is test data, eventually this should communicate
+    ## with the server to register a new user.
+    ukey_path = Path(MODULE_DIR + "/../server/testingData/8270950596653088861-U.pem")
+    rkey_path = Path(MODULE_DIR + "/../server/testingData/8270950596653088861-R.pem")
+    data = {"id": "8270950596653088861", \
+            "group": "3911253593270387734", \
+            "public_key": ukey_path.read_text(), \
+            "private_key": rkey_path.read_text() }
+    
+    ## TODO: Don't save user to local storage until we have "new user"
+    ## functionality in server working.
+    #fout = open(SAVE_PATH, "w")
+    #json.dump(data)
+    #fout.close()
+    
+    return json_to_user(data)
 
 def main():
     ## Initialize user.
@@ -32,17 +46,26 @@ def main():
         except json.JSONDecodeError:
             user = new_user();
         else:
-            if "id" in data and "public_key" in data and "private_key" in data:
-                user = User(data["in"], data["public_key"], data["private_key"])
+            if "id" in data and  "public_key" in data and \
+               "private_key" in data and "group" in data:
+                try:
+                    user = json_to_user(data)
+                except ValueError:
+                    user = new_user()
             else:
                 user = new_user()
         fin.close()
     else:
         user = new_user()
 
-    ## Initialize window.
-    window = Tk()
-    #window.mainloop()
+    print(user.id)
+    print("\n")
+    print(user.public_key)
+    print("\n")
+    print(user.private_key)
+    print("\n")
+    print(user.group)
+    print("\n")
 
 if __name__ == "__main__":
     main()
